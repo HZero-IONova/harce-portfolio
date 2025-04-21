d87cac71e9e1e8e61e4d008d128cb3b3b1089432
unknown <ionova.io@outlook.com>
2025-04-21 15:15:36 -0700

    Update - Social Links

--- components/Social.jsx
+++ components/Social.jsx
@@ -1,27 +1,31 @@
 "use client";
 
 import Link from "next/link";
-import {FaGithub, FaLinkedinIn, FaFacebookF, FaInstagram} from "react-icons/fa"
+import {
+  FaGithub,
+  FaLinkedinIn,
+  FaFacebookF,
+  FaInstagram,
+} from "react-icons/fa";
 
 const socials = [
-    { icon: <FaGithub />, path: "" },
-    { icon: <FaLinkedinIn />, path: "" },
-    { icon: <FaFacebookF />, path: "" },
-    { icon: <FaInstagram />, path: "" },
+  { icon: <FaGithub />, path: "https://github.com/HZero-IONova" },
+  { icon: <FaLinkedinIn />, path: "www.linkedin.com/in/rhag" },
+  { icon: <FaInstagram />, path: "https://www.instagram.com/ionova.io/" },
 ];
 
-const Social = ({containerStyles, iconStyles}) => {
-    return (
-        <div className={ containerStyles }>
-            {socials.map((item, index)=> {
-            return (
-            <Link key={index} href={item.path} className={iconStyles}>
-                {item.icon}
-            </Link>
-            );
-            })}
-        </div>
-    );
-}
+const Social = ({ containerStyles, iconStyles }) => {
+  return (
+    <div className={containerStyles}>
+      {socials.map((item, index) => {
+        return (
+          <Link key={index} href={item.path} className={iconStyles}>
+            {item.icon}
+          </Link>
+        );
+      })}
+    </div>
+  );
+};
 
 export default Social;
