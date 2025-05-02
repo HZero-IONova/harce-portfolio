ab0c17ec0ca57d3d5eb6486a0d1b31d9ca1d9efd
unknown <ionova.io@outlook.com>
2025-05-02 14:06:26 -0700

    Photo Update

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -23,7 +23,7 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute"
         >
           <Image
-            src="/assets/profilePhoto.JPG"
+            src="/assets/profilePhoto.jpg"
             priority
             quality={100}
             fill
